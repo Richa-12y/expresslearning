ba50df9244908c905e855d1cdb672230e2c59b2b
Richa-12y <kricha000@gmail.com>
2023-02-13 18:15:38 +0530

    express js learning in class

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * This can be used as a starting file
+ */
+
+const express = require("express");
+
+console.log(typeof express); //function
+
+const app = express();
+console.log(typeof app); //function
+
+/**
+ * app is the core of the whole express application
+ * Everything revolve around app
+ */
+
+/**
+ * Accepting the client request
+ */
+app.get("/", (req, res) => {
+  res.status(200).send("Hello Coder ! 👩‍💻👨‍💻");
+});
+
+/**
+ * Starting a server
+ */
+
+app.listen(1315, () => {
+  console.log("Server has started on 1315");
+});
